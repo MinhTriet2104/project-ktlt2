37f1e56a1141a1955f4d90943858d9d47a4a504a
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-05-09 20:09:15 +0700

    Tính toán ngày mượn và ngày trả

--- PHIEUMUON.cpp
+++ PHIEUMUON.cpp
@@ -2,42 +2,54 @@
 
 int	PHIEUMUON::getSoPhieuMuon()
 {
-	return iSoPhieuMuon;
-}
-string PHIEUMUON::getNgayMuon()
-{
-	return sNgayMuon;
-}
-string PHIEUMUON::getNgayTra()
-{
-	return sNgayTra;
+	return this->iSoPhieu;
 }
 int PHIEUMUON::getTinhTrangPhieuMuon()
 {
 	return iTinhTrangPhieuMuon;
 }
-void PHIEUMUON::setSoPhieuMuon(int iSoPhieuMuon)
-{
-	this->iSoPhieuMuon = iSoPhieuMuon;
-}
-void PHIEUMUON::setNgayMuon(string sNgayMuon)
-{
-	this->sNgayMuon = sNgayMuon;
-}
-void PHIEUMUON::setNgayTra(string sNgayTra)
-{
-	this->sNgayTra = sNgayTra;
-}
 void PHIEUMUON::setTinhTrangPhieuMuon(int iTinhTrangPhieuMuon)
 {
 	this->iTinhTrangPhieuMuon = iTinhTrangPhieuMuon;
 }
-void PHIEUMUON::setPhieumuon(int iSoPhieuMuon, string sMaBanDoc,
-	string sNgayMuon, string sNgayTra, int iTinhTrangPhieuMuon)
-{
 
-	this->sNgayMuon = sNgayMuon;
-	this->sNgayTra = sNgayTra;
-	this->iSoPhieuMuon = iSoPhieuMuon;
-	this->iTinhTrangPhieuMuon = iTinhTrangPhieuMuon;
+Date PHIEUMUON::tinhNgayTra() {
+	Date xNgayTra = this->xNgayMuon;
+	xNgayTra.iNgay += 7;
+	switch (xNgayTra.iThang) {
+	case 2:
+		if (xNgayTra.iNam % 400 == 0 || xNgayTra.iNam % 4 == 0 && xNgayTra.iNam % 100 != 0) {
+			if (xNgayTra.getNgay() > 29) {
+				xNgayTra.iNgay -= 29;
+				xNgayTra.iThang++;
+			} else {
+				xNgayTra.iNgay -= 28;
+				xNgayTra.iThang++;
+			}
+		}
+		break;
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		if (xNgayTra.getNgay() > 31) {
+			xNgayTra.iNgay -= 31;
+			xNgayTra.iThang++;
+		}
+		if (xNgayTra.iThang > 12) {
+			xNgayTra.iThang = 1;
+			xNgayTra.iNam++;
+		}
+		break;
+	default:
+		if (xNgayTra.getNgay() > 30) {
+			xNgayTra.iNgay -= 30;
+			xNgayTra.iThang++;
+		}
+		break;
+	}
+	return xNgayTra;
 }
--- PHIEUMUON.h
+++ PHIEUMUON.h
@@ -1,89 +1,92 @@
 #pragma once
-#include"BANDOC.h"
+#include "Date.h"
+#include "BANDOC.h"
+#include "SACH.h"
 
 class PHIEUMUON
 {
 private:
-	int	iSoPhieuMuon;
-	string sMaSach;
-	string sNgayMuon;
-	string sNgayTra;
-	int iTinhTrangPhieuMuon;
+	static int iSoPhieuMuon;
+	int iSoPhieu;
 	BANDOC xBD;
+	SACH xSach;
+	string sMaBD;
+	string sMaSach;
+	Date xNgayMuon;
+	Date xNgayTra;
+	int iTinhTrangPhieuMuon;
 public:
-	PHIEUMUON()
-	{
-		
-		sNgayMuon = "";
-		sNgayTra = "";
-		iSoPhieuMuon = 0;
-		iTinhTrangPhieuMuon = 0;
+	PHIEUMUON() {
+		iSoPhieuMuon++;
+		this->iSoPhieu = iSoPhieuMuon;
+		this->iTinhTrangPhieuMuon = 1;
 
 	}
 
-	PHIEUMUON(int iSoPhieuMuon, string sNgayMuon, string sNgayTra, int iTinhTrangPhieuMuon,
-		string sHoTen, string sKhoa, string sMaBanDoc, time_t tNgayThamGia)
-	{
-		
-		this->sNgayMuon = sNgayMuon;
-		this->sNgayTra = sNgayTra;
-		this->iSoPhieuMuon = iSoPhieuMuon;
-		this->iTinhTrangPhieuMuon = iTinhTrangPhieuMuon;
+	PHIEUMUON(string sMaBD, string sMaSach) : xBD(sMaBD), xSach(sMaSach)  {
+		iSoPhieuMuon++;
+		this->iSoPhieu = iSoPhieuMuon;
+		this->sMaBD = sMaBD;
+		this->sMaSach = sMaSach;
+		//this->xNgayMuon.iNgay = 25;
+		//this->xNgayMuon.iThang = 5;
+		//this->xNgayMuon.iNam = 2019;
+		this->xNgayTra = this->tinhNgayTra();
+		this->iTinhTrangPhieuMuon = 1;
 	}
 	
 	void setMaSV(string sMaSV);
 	void setMaGV(string sMaGV);
 	int	getSoPhieuMuon();
-	string getNgayMuon();
-	string getNgayTra();
+	Date getNgayMuon();
+	Date tinhNgayTra();
 	int getTinhTrangPhieuMuon();
-	void setSoPhieuMuon(int iSoPhieuMuon);
-	void setNgayMuon(string sNgayMuon);
-	void setNgayTra(string sNgayTra);
+	void setSoPhieuMuon(int iSoPhieu);
 	void setTinhTrangPhieuMuon(int iTinhTrangPhieuMuon);
-	void setPhieumuon(int iSoPhieuMuon, string sMaBanDoc,
-		string sNgayMuon, string sNgayTra, int iTinhTrangPhieuMuon);
 	//Dinh nghia toan tu nhap/xuat:
-	friend istream& operator >> (istream&is, PHIEUMUON xPM)
-	{	
-		cout << "Ngay muon : ";
+	friend istream& operator >> (istream&is, PHIEUMUON &xPM) {	
+		cout << "Ma Ban doc: ";
 		rewind(stdin);
-		getline(cin, xPM.sNgayMuon);
-		cout << "Ngay tra: ";
+		getline(cin, xPM.sMaBD);
+
+		cout << "Ma Sach: ";
 		rewind(stdin);
-		getline(cin, xPM.sNgayTra);
-		cout << "So phieu muon : ";
-		cin >> xPM.iSoPhieuMuon;
-		cout << "Tinh trang phieu muon: ";
-		cin >> xPM.iTinhTrangPhieuMuon;
+		getline(cin, xPM.sMaSach);
+
 		return is;
 	}
-	friend ostream& operator << (ostream&os, PHIEUMUON xPM)
-	{
-		os << "Ngay muon : " << xPM.sNgayMuon << endl;
-		os << "Ngay tra : " << xPM.sNgayTra << endl;
-		os << "So phieu muon : " << xPM.iSoPhieuMuon << endl;
+	friend ostream& operator << (ostream&os, PHIEUMUON &xPM) {
+		os << "So phieu muon : " << xPM.iSoPhieu << endl;
+		os << "Ma Ban doc: " << xPM.sMaBD << endl;
+		os << "Ma Sach: " << xPM.sMaSach << endl;
+		os << "Ngay muon : " << xPM.xNgayMuon;
+		os << "Ngay tra : " << xPM.xNgayTra;
 		os << "Tinh trang phieu muon : " << xPM.iTinhTrangPhieuMuon << endl;
 		return os;
 	}
 	//Doc va ghi file
 	friend ifstream& operator >> (ifstream &ifs, PHIEUMUON &xPM) {
-		getline(ifs, xPM.sNgayMuon, '#');
+		ifs >> xPM.iSoPhieu;
+		ifs.ignore(1);
+		getline(ifs, xPM.sMaBD, '#');
+		getline(ifs, xPM.sMaSach, '#');
 
-		getline(ifs, xPM.sNgayTra, '#');
+		ifs >> xPM.xNgayMuon;
+		ifs.ignore(1);
 
-		ifs >> xPM.iSoPhieuMuon;
-			ifs.ignore(1);
-			ifs >> xPM.iTinhTrangPhieuMuon;
-			ifs.ignore(1);
+		ifs >> xPM.xNgayTra;
+		ifs.ignore(1);
+
+		ifs >> xPM.iTinhTrangPhieuMuon;
+
 		return ifs;
 	}
 
 	friend ofstream& operator << (ofstream &ofs, PHIEUMUON &xPM) {
-		ofs << xPM.sNgayMuon << '#';
-		ofs << xPM.sNgayTra << '#';
+		ofs << xPM.xNgayMuon << '#';
+		ofs << xPM.xNgayTra << '#';
 		ofs << xPM.iSoPhieuMuon << '#';
-		ofs << xPM.iTinhTrangPhieuMuon << '#';
+		ofs << xPM.iTinhTrangPhieuMuon;
 		return ofs;
 	}
 	~PHIEUMUON() {};
